229d88637930142c91a17c07207f383a56f6015c
Leica913 <toruneko1996913@gmail.com>
2021-02-16 09:28:46 +0000

    [Start] 4-4

---
+++ meshiterro4-4/config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '3794f366bccf69e73d1aae766b363cc46a72b9ad4f7c69c758e52966f547c7b7d39c4baac9f9ff683bf1e279f6074074e7a3f0e4792c152cb880950b9c0952c6'
