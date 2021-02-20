59c607c225ff1dc93a987cf2adf0dc90b99a1c5f
Leica913 <toruneko1996913@gmail.com>
2021-02-20 09:19:38 +0000

    [Finish] 4-8

--- meshiterro4-8/config/initializers/application_controller_renderer.rb
+++ meshiterro4-8/config/initializers/application_controller_renderer.rb
@@ -6,5 +6,3 @@
 #     https: false
 #   )
 # end
-
-Refile.secret_key = '3794f366bccf69e73d1aae766b363cc46a72b9ad4f7c69c758e52966f547c7b7d39c4baac9f9ff683bf1e279f6074074e7a3f0e4792c152cb880950b9c0952c6'
--- meshiterro4-8/config/routes.rb
+++ meshiterro4-8/config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :users
+  resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'homes#top'
-  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
-    resource :favorites, only: [:create, :destroy]
-    resources :post_comments, only: [:create, :destroy]
-  end
-  resources :users, only: [:show, :edit, :update]
 end
